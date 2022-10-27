78509478dcbb4f49638061a70aa383a5ea2944b7
kentrietran <87349870+kentrietran@users.noreply.github.com>
2022-10-26 20:32:52 -0400

    Added more to server

---
+++ server/routes/routes.js
@@ -0,0 +1,8 @@
+
+const controllers = require('../controllers/controller.js');
+const router = require('express').Router();
+
+router.route("/hello").get(controllers.hello);
+
+module.exports = router;
+
