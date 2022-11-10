6ff22145e2656bf1ed5adb32ec4abb468b3fcc99
kentrietran <87349870+kentrietran@users.noreply.github.com>
2022-11-09 20:43:50 -0500

    GET and POST

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -1,5 +1,17 @@
+const data = require("../data");
 module.exports = {
     hello: (req, res) => {
         res.json({"message": "Hello World!"});
     },
+    sources: (req, res) => {
+        res.json({"message": "Sources Snatched (success!)", data: data.sources});
+    },
+    add_source: (req, res) => {
+        data.sources.push(req.body.source);
+        res.json({"message": "Sources Added", data: data.sources});
+    },
+    delete_source: (req, res) => {
+        data.sources = data.sources.filter(delete => delete.id!==req.params.id);
+        res.json({"message": "Sources Deleted", data: data.sources});
+    }
 };
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,7 +2,12 @@
 const controllers = require('../controllers/controller.js');
 const router = require('express').Router();
 
-router.route("/hello").get(controllers.hello);
+router.routes("/hello").get(controllers.hello);
+
+router.route("/api/sources").get(controller.sources);
+router.route("/api/add_source").post(controller.add_source);
+router.route("/api/delete_source/:id").post(controller.delete_source);
 
 module.exports = router;
+
 
