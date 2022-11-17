84d2d54a2c24575c4641a77c26a8ff7e0708ee00
Bech <tobechionwuka@gmail.com>
2022-11-16 20:56:46 -0500

    added stuff to server folder

---
+++ server/models/SourceModel.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// WRITE THE SCHEMA HERE
+//database of spec sources
+//scheme tells database that a spec source is a thing with an id, name, and email
+const sourceSchema = new Schema({
+    id: { type: Number, required: true }, //required makes the field necessary to fill out
+    name: { type: String, required: true},
+    email: { type: String, required: true }
+})
+
+const sources = mongoose.model('spec_sources', sourceSchema);
+
+module.exports = sources;
