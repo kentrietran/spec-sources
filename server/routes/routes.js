5422d1931073d404c06a43038b3ed14759086ba7
Bech <tobechionwuka@gmail.com>
2022-11-28 19:16:50 -0500

    new

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -4,9 +4,9 @@ const router = require('express').Router();
 
 router.routes("/hello").get(controllers.hello);
 
-router.route("/api/sources").get(controller.sources);
-router.route("/api/add_source").post(controller.add_source);
-router.route("/api/delete_source/:id").post(controller.delete_source);
+router.route("/sources").get(controller.sources);
+router.route("/add_source").post(controller.add_source);
+router.route("/delete_source/:id").get(controller.delete_source);
 
 module.exports = router;
 
