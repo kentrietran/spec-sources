8f46ba9e59423ed779edf618701d638dc0406d7e
Bech <tobechionwuka@gmail.com>
2022-12-07 20:33:39 -0500

    updated config

--- server/models/config.js
+++ server/models/config.js
@@ -1,14 +1,11 @@
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({path: path.resolve(__dirname, '../../.env')});
 
 const dbUser = process.env.DBUSER;
 const dbName = process.env.DBNAME;
 const dbPassword = process.env.DBPASSWORD;
 const discardAfterXDays = 14;
-//const URI = `mongodb+srv://${dbUser}:${dbPassword}@${dbName}.7mfnpbj.mongodb.net/?retryWrites=true&w=majority`;
-
-
-const URI = `mongodb+srv://toonwuka:${dbPassword}@specsourcepractice.7mfnpbj.mongodb.net/?retryWrites=true&w=majority
-`;
+const URI = `mongodb+srv://${dbUser}:${dbPassword}@${dbName}.7mfnpbj.mongodb.net/?retryWrites=true&w=majority`;
 
 
 module.exports = {
