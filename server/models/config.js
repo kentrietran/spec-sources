b7bbd2c2ce5ab9ec6808ee3ac8b97d2273635708
Bech <tobechionwuka@gmail.com>
2022-12-12 16:25:29 -0500

    updated config file to have collection name

--- server/models/config.js
+++ server/models/config.js
@@ -4,8 +4,9 @@ require('dotenv').config({path: path.resolve(__dirname, '../../.env')});
 const dbUser = process.env.DBUSER;
 const dbName = process.env.DBNAME;
 const dbPassword = process.env.DBPASSWORD;
+const dbCollection = process.env.DBCOLLECTION;
 const discardAfterXDays = 14;
-const URI = `mongodb+srv://${dbUser}:${dbPassword}@specsourcepractice.7mfnpbj.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+const URI = `mongodb+srv://${dbUser}:${dbPassword}@${dbName}.7mfnpbj.mongodb.net/${dbCollection}?retryWrites=true&w=majority`;
 
 
 module.exports = {
